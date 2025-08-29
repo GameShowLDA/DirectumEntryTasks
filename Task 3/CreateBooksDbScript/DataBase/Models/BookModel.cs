97f2715af0afc046221a9edd9ac3088f379cf014
Daniil Lukashin <39368626+GameShowLDA@users.noreply.github.com>
2025-08-29 15:17:33 +0400

    Скрипт по созданию БД.

---
+++ Task 3/CreateBooksDbScript/DataBase/Models/BookModel.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CreateBooksDbScript.DB.Models
+{
+  /// <summary>
+  /// Модель книги.
+  /// </summary>
+  internal class BookModel
+  {
+    /// <summary>
+    /// Уникальный идентификатор книги.
+    /// </summary>
+    [Key]
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Наименование книги.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Тираж книги.
+    /// </summary>
+    public int PrintRun { get; set; }
+
+    /// <summary>
+    /// Уникальный идентификатор автора книги.
+    /// </summary>
+    public int AuthorId { get; set; }
+
+    /// <summary>
+    /// Уникальный идентификатор издательства книги.
+    /// </summary>
+    public int PublishingHouseId { get; set; }
+  }
+}
---
+++ Task 3/CreateBooksDbScript/Program.cs
@@ -0,0 +1,13 @@
+﻿using CreateBooksDbScript.DataBase;
+
+namespace CreateBooksDbScript
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      new AppDbContext();
+      Console.WriteLine($"Путь к базе данных:{AppDbContext.DbPath}");
+    }
+  }
+}
